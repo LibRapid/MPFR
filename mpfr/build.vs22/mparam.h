d8427559eae90de1fade2af0bbd838ed83530170
Toby Davis <52111185+Pencilcaseman@users.noreply.github.com>
2023-10-20 23:18:10 +0100

    Update mparam.h

--- mpfr/build.vs22/mparam.h
+++ mpfr/build.vs22/mparam.h
@@ -42,8 +42,6 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 #	endif
 #endif
 
-#if defined( LIBRAPID_64BIT )
-
 // We shouldn't always use this one, but it works...
 // if you really need the best performance, install MPFR yourself >:)
 include "../src/x86_64/mparam.h"
